170cd53101220b6458b36a4320e7541d9c93ac16
website-company <mail@website-company.com>
2016-12-14 21:20:57 -0500

    stage 2

--- cwd/html/header.php
+++ cwd/html/header.php
@@ -8,3 +8,18 @@
     <link type="text/css" href="/public/css/main.css" rel="stylesheet">
 </head>
 <body>
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="rol-sm-4">
+                <img title="Palm Beach Daycare and Learning Center" src="/public/images/palm-bay-daycare-logo.png" alt="Palm Beach Daycare Logo">
+            </div>
+            <div class="rol-sm-4">
+
+            </div>
+            <div class="rol-sm-4">
+
+            </div>
+        </div>
+    </div>
+</header>
